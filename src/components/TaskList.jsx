2ed0036a1a470dfb123b52fbf44c050e9e62b06a
S <s@sample.com>
2025-08-30 12:16:35 +0900

    複合的なコンポーネント。表示順やローディング中の改善。

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,22 +1,66 @@
 import Task from "./Task";
+import PropTypes from "prop-types";
+TaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+  loading: false,
+};
 
 export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
   };
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>読み込み中</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
 
   if (loading) {
-    return <div className="list-items">読み込み中です</div>;
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items">タスクはありません</div>;
+    return (
+      <div className="list-items" key={"empty"} data-testid="empty">
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <p className="title-message">タスクはありません</p>
+          <p className="subtitle-message">座ってリラックスしてください</p>
+        </div>
+      </div>
+    );
   }
+
+  const taskInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
 
   return (
     <div className="list-items">
-      {tasks.map((task) => (
+      {taskInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
