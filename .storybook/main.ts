3e5c427e653b03b964e9955577df28530c110706
S <s@sample.com>
2025-08-30 11:01:06 +0900

    simple component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
+  // デフォルトでは src 配下の .mdx, .stories. ファイルが対象
+  // src/stories 配下にはサンプルのストーリーファイルがある
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // 次の通りにすれば、ディレクトリを特定できる
+  // stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
@@ -12,5 +16,9 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+  // チュートリアルには以下のコードがあるがデフォルトには無い設定。無くても Docs は表示されている。
+  // docs: {
+  //   autodocs: 'tag',
+  // },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,11 @@
+import '../src/index.css'; // storybook でのコンポーネントの表示に適用するcss
+
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    // 指定の Props を自動的に Storybook の「Actions」タブにイベント表示する設定。
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
